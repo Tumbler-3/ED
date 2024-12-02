9be93404ab5db4c672295b97011cbfd3837bde27
Tamerlan <topkek164@gmail.com>
2024-12-03 01:23:31 +0600

    User Model update

--- apps/User/models.py
+++ apps/User/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(blank=True,null=True, unique=True)
+    email = models.EmailField(blank=False,null=False, unique=True)
     
     def __str__(self):
         return self.username
