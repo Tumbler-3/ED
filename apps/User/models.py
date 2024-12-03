8ee6846607b1aaa5564d220d14f4a06f9791aafb
Tamerlan <topkek164@gmail.com>
2024-12-03 11:49:10 +0600

    Time Field changed to timedatefield

--- apps/User/models.py
+++ apps/User/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(verbose_name='Email',blank=False,null=False, unique=True)
-    created_at = models.TimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.username
